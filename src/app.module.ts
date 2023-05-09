f1e9b58230086791a13c5a68b0c973198a271f37
Ihor <ionaigor10@gmail.com>
2023-05-09 23:43:07 +0300

    added typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { PostsModule } from './posts/posts.module';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from '@hapi/joi';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import * as Joi from '@hapi/joi';
         PORT: Joi.number(),
       }),
     }),
+    DatabaseModule,
   ],
   controllers: [],
   providers: [],
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,19 +1,22 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreatePostDto } from './dto/createPost.dto';
-import { Post } from './posts.interface';
 import { UpdatePostDto } from './dto/updatePost.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Post } from './post.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class PostsService {
-  private lastPostId = 0;
-  private posts: Post[] = [];
+  constructor(
+    @InjectRepository(Post) private postRepository: Repository<Post>,
+  ) {}
 
   getAllPosts() {
-    return this.posts;
+    return this.postRepository.find();
   }
 
-  getPostById(id: number) {
-    const post = this.posts.find((post) => post.id === id);
+  async getPostById(id: number) {
+    const post = await this.postRepository.findOne({ where: { id } });
     if (post) {
       return post;
     }
@@ -21,29 +24,29 @@ export class PostsService {
   }
 
   replacePost(id: number, post: UpdatePostDto) {
-    const postIndex = this.posts.findIndex((post) => post.id === id);
-    if (postIndex > -1) {
-      this.posts[postIndex] = post;
-      return post;
-    }
+    // const postIndex = this.posts.findIndex((post) => post.id === id);
+    // if (postIndex > -1) {
+    //   this.posts[postIndex] = post;
+    //   return post;
+    // }
     throw new HttpException('Post not found', HttpStatus.NOT_FOUND);
   }
 
   createPost(post: CreatePostDto) {
-    const newPost = {
-      id: ++this.lastPostId,
-      ...post,
-    };
-    this.posts.push(newPost);
-    return newPost;
+    // const newPost = {
+    //   id: ++this.lastPostId,
+    //   ...post,
+    // };
+    // this.posts.push(newPost);
+    // return newPost;
   }
 
   deletePost(id: number) {
-    const postIndex = this.posts.findIndex((post) => post.id === id);
-    if (postIndex > -1) {
-      this.posts.splice(postIndex, 1);
-    } else {
-      throw new HttpException('Post not found', HttpStatus.NOT_FOUND);
-    }
+    // const postIndex = this.posts.findIndex((post) => post.id === id);
+    // if (postIndex > -1) {
+    //   this.posts.splice(postIndex, 1);
+    // } else {
+    //   throw new HttpException('Post not found', HttpStatus.NOT_FOUND);
+    // }
   }
 }
